946e249c8a39a4775f642617cb3fdb103a72ca40
redaphid <iam@aaronherres.com>
2016-03-05 21:47:38 -0700

    transmission settings

--- services/transmission/run.sh
+++ services/transmission/run.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 docker rm -f transmission
+cp settings.json config/
 docker run \
     --name transmission \
     --restart="always" \
     -v $PWD/config:/config \
-    -p 9091 \    
+    -v $HOME/Downloads:/downloads \
+    -v $HOME/media:/done \
+    -p 9091 \
     redaphid/transmission:latest
