370fe6bde4be16416aec933c632e745406575a87
redaphid <iam@aaronherres.com>
2016-03-07 00:05:03 -0700

    escapes also help

--- services/transmission/run.sh
+++ services/transmission/run.sh
@@ -4,7 +4,7 @@ cp settings.json config/
 docker run \
     --name transmission \
     --restart="always" \
-    --net="host"
+    --net="host" \
     -v $PWD/config:/config \
     -v $MEDIA_LOCAL_DIRECTORY/downloads:/downloads \
     -v $MEDIA_LOCAL_DIRECTORY/incoming:/done \
