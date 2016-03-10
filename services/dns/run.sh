2f8e7b153b03ba5f9875651323d308072743f131
redaphid <iam@aaronherres.com>
2016-03-10 03:21:20 -0700

    omg pxe

--- services/dns/run.sh
+++ services/dns/run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sudo ip link set enx3c18a0400871 up
 sudo ip addr add 10.0.0.1/16 dev enx3c18a0400871
 docker rm -f dns
 sudo ./setup-conf.sh
@@ -10,6 +9,7 @@ docker run \
     --restart="always" \
     -d \
     -p 53:53/udp \
+    -p 67:67/udp \
     -p 8080:8080 \
     -v $PWD/config/dnsmasq.conf:/etc/dnsmasq.conf \
     -v $PWD/config/pxe:/pxe \
--- services/dns/setup-conf.sh
+++ services/dns/setup-conf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -e "s|{{IP}}|10.0.1.5|" \
+sed -e "s|{{IP}}|10.0.0.1|" \
   <./dnsmasq.conf.template >./config/dnsmasq.conf
