f9d5cbfa717a86d5b38f3346036deaa358ef7457
redaphid <iam@aaronherres.com>
2016-03-10 05:28:18 -0700

    i386 pxe. Docker needs 64 bit. fail

--- services/dns/setup-pxe.sh
+++ services/dns/setup-pxe.sh
@@ -3,7 +3,7 @@ mkdir -p ./config/pxe
 cd ./config/pxe
 
 if [ ! -f netboot.tar.gz ]; then
-  wget http://archive.ubuntu.com/ubuntu/dists/wily/main/installer-i386/current/images/netboot/netboot.tar.gz
+  wget http://archive.ubuntu.com/ubuntu/dists/wily/main/installer-amd64/current/images/netboot/netboot.tar.gz
 fi
 
 # if [ ! -f grubnetx64.efi.signed ]; then
