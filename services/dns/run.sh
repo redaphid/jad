a05dfea51ee043bbb26686c03a6fb4b21d1f9c61
redaphid <iam@aaronherres.com>
2016-03-10 23:23:11 -0700

    desperately fighitng dns

--- services/dns/run.sh
+++ services/dns/run.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
+LAN_IFACE=enp37s0
+ip addr flush dev $LAN_IFACE
+ip route flush dev $LAN_IFACE
+ip link set $LAN_IFACE down
+
+ip link set $LAN_IFACE up
+ip addr add 10.0.0.1/16 dev $LAN_IFACE
+ip route add default via 10.0.0.2
 
 docker rm -f dns
 sudo ./setup-conf.sh
 docker run \
-    --privileged \
     --name dns \
     --net="host" \
+    --privileged \
     --restart="always" \
     -d \
     -p 53:53/udp \
--- services/dns/setup-conf.sh
+++ services/dns/setup-conf.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo ip addr add 10.0.0.1/16 dev enx3c18a0400871
 sed -e "s|{{IP}}|10.0.0.1|" \
   <./dnsmasq.conf.template >./config/dnsmasq.conf
